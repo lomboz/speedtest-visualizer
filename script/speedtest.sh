3cec98342b5e52f64960e91337a2822208082738
haschneider <hans.schneider@adesso.de>
2020-05-29 22:07:18 +0200

    fix script

--- script/speedtest.sh
+++ script/speedtest.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
-TARGET_DIR=../data
+TARGET_DIR=/var/www/html/speedtest-visualizer/data
 
 echo "run speedtest on "$TIMESTAMP
 
-./speedtest-cli --json >> $TARGET_DIR/speedtest_$TIMESTAMP.json
+/var/www/html/speedtest-visualizer/script/speedtest-cli --json >> $TARGET_DIR/speedtest_$TIMESTAMP.json
 
 echo "speedtest_$TIMESTAMP.json" >> $TARGET_DIR/index.txt
