5d73dee8456c52b3133f2065b276121984c6c96a
hschneider <lomboz92@googlemail.com>
2020-07-04 14:39:15 +0200

    final running version

--- script/speedtest.sh
+++ script/speedtest.sh
@@ -2,9 +2,20 @@
 
 TIMESTAMP=$(date +'%Y-%m-%d_%H-%M-%S')
 TARGET_DIR=/var/www/html/speedtest-visualizer/data
+CALL_PATH=$(pwd)
 
 echo "run speedtest on "$TIMESTAMP
 
-/var/www/html/speedtest-visualizer/script/speedtest-cli --json >> $TARGET_DIR/speedtest_$TIMESTAMP.json
+#/var/www/html/speedtest-visualizer/script/speedtest-cli --json >> $TARGET_DIR/speedtest_$TIMESTAMP.json
+speedtest --server 33397 --json >> $TARGET_DIR/speedtest_$TIMESTAMP.json
 
-echo "speedtest_$TIMESTAMP.json" >> $TARGET_DIR/index.txt
+# NUr Ergebnisse der letzten Wochen
+echo "daten aufräumen"
+find $TARGET_DIR -depth -mtime +7 -exec rm -f '{}' \;
+echo "index der Dateien anlegen"
+cd $TARGET_DIR
+find  -type f \( -name "*.json" \) | sort > $TARGET_DIR/index.txt
+
+cd $CALL_PATH
+#echo "speedtest_$TIMESTAMP.json" >> $TARGET_DIR/index.txt
+
